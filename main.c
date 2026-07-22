c3cfed2b29109be15eec495e74d5a8c4397c1ac6
T4R1N <contactotarin@gmail.com>
2026-07-22 13:28:18 -0600

    Feat: Add explicit process name
    Executable is now named "HTTP Server". This was done to ease lookup
    while analyzing the process' activity with tools like netstat.

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "server.h"
 #include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  if (argc > 1)
+    exit(0);
+
+  strcpy(argv[0], "HTTP Server");
+
   struct Server server =
       server_init(AF_INET, 8008, SOCK_STREAM, 0, 10, INADDR_ANY, server_launch);
 
